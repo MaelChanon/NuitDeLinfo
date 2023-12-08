5b1406d6deb7d8ed0a6a50b874d94137b14d4947
Sofia <sf.hazami@gmail.com>
2023-12-08 07:16:47 +0100

    recuperation

--- src/back/threeEarth.js
+++ src/back/threeEarth.js
@@ -1,15 +1,6 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import * as dat from "lil-gui";
 import data from "./bootstrap"
 import {updateInterface,getHtmlTemplate} from './ihm'
-/** 
- * Base
- */
-// Debug
-const gui = new dat.GUI({
-  width: 390,
-});
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -20,12 +11,92 @@ const scene = new THREE.Scene();
 //Textures
 const textureLoader = new THREE.TextureLoader();
 const earthTexture = textureLoader.load("/textures/map-4818844_1280.jpg");
+const starTexture = textureLoader.load("/textures/star_08.png");
 
-const geometry = new THREE.SphereGeometry( 15, 32, 16 ); 
-const material = new THREE.MeshBasicMaterial( { 
-map :earthTexture } ); 
-const sphere = new THREE.Mesh( geometry, material );
+const geometrySphere = new THREE.SphereGeometry( 15, 32, 16 ); 
+const materialSphere = new THREE.MeshBasicMaterial( { map :earthTexture } ); 
+const sphere = new THREE.Mesh( geometrySphere, materialSphere );
+sphere.rotation.x = 0.63
+sphere.rotation.y = -1.78
 scene.add( sphere );
+
+/**
+ * Galaxy
+ */
+const parameters = {};
+parameters.count = 1000;
+parameters.size = 2.5;
+parameters.radius = 500;
+parameters.branches = 3;
+parameters.spin = 1;
+parameters.randomness = 20;
+parameters.randomnessPower = 2.6;
+
+let geometry = null;
+let material = null;
+let points = null;
+
+const generateGalaxy = () => {
+
+//   /**
+//    * Geometry
+//    */
+  geometry = new THREE.BufferGeometry();
+  const positions = new Float32Array(parameters.count * 3);
+
+  for (let i = 0; i < parameters.count; i++) {
+    const i3 = i * 3;
+
+    // Position
+    const radius = Math.random() * parameters.radius; //la distance entre le centre et le point le plus éloigné (rayon)
+    const spinAngle = radius * parameters.spin; //la rotation autour du centre
+    const brancheAngle =
+      ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
+
+    const randomX =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      (Math.random() < 0.5 ? 1 : -1) *
+      parameters.randomness *
+      radius; //le random de la position en x (randomPower pour que les points soient plus proches du centre)
+    const randomY =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      (Math.random() < 0.5 ? 1 : -1) *
+      parameters.randomness *
+      radius;
+    const randomZ =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      (Math.random() < 0.5 ? 1 : -1) *
+      parameters.randomness *
+      radius;
+
+    positions[i3] = Math.cos(brancheAngle + spinAngle) * radius + randomX;
+    positions[i3 + 1] = randomY;
+    positions[i3 + 2] = Math.sin(brancheAngle + spinAngle) * radius + randomZ;
+
+  }
+  geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+
+//   /**
+//    * Material
+//    */
+  material = new THREE.PointsMaterial({
+    size: parameters.size,
+    sizeAttenuation: true,
+    transparent: true,
+    alphaMap: starTexture,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+    color: "white"
+  });
+
+  /**
+   * Points
+   */
+  points = new THREE.Points(geometry, material);
+  scene.add(points);
+};
+
+generateGalaxy();
 
 /**
  * Sizes
@@ -59,14 +130,10 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.x = 3;
-camera.position.y = 30;
-camera.position.z = 3;
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 50;
 scene.add(camera);
-
-// Controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
 
 /**
  * Renderer
@@ -86,9 +153,8 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
   // Update controls
-  controls.update();
-  sphere.rotation.y = elapsedTime * 0.1;
-  //sphere.rotation.z = -elapsedTime * 0.01;
+  sphere.rotation.y = elapsedTime * 0.2;
+  points.rotation.y = elapsedTime * 0.1;
 
 
   // Render
